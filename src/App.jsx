c13d811b0c290f7a04b981673b62295a79eec274
shrouk <shrmhfathy.99@gmail.com>
2024-12-07 23:23:35 +0200

    package changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./styles/App.css";
 import ForgetPassword from "./pages/log-in/ForgetPassword";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PasswordReset from "./pages/log-in/PasswordReset";
 import NewPassword from "./pages/log-in/NewPassword";
 import PasswordReseted from "./pages/log-in/PasswordReseted";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<ForgetPassword />} />
         <Route path="/OTP" element={<PasswordReset />} />
         <Route path="/setpassword" element={<NewPassword />} />
         <Route path="/PaswordReseted" element={<PasswordReseted />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
